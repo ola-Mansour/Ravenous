e14ff80414a94d49a2db6a5b43d568ac68ef1389
ola-Mansour <oulamanso0ur@gmail.com>
2020-08-14 18:05:01 +0300

    Updates

---
+++ precache-manifest.d7e9aa1ffa3440ce02a43ee2992d153b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58dd131ad1f77b21cf117f02344c19b",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "2d222b28b2422d600542",
+    "url": "/Ravenous/static/css/main.a4527ef9.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/Ravenous/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d222b28b2422d600542",
+    "url": "/Ravenous/static/js/main.19885b94.chunk.js"
+  },
+  {
+    "revision": "cd33480439a159f358bd",
+    "url": "/Ravenous/static/js/runtime-main.9bb2f265.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
